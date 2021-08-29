085ba84cce826ef9dbef689caeb6dca13cbc25d7
chandan <chandan.krc361991@gmail.com>
2021-08-29 12:56:30 +0530

    Creating package and class

---
+++ CredentialCreatorApplication/src/com/greatlearning/corejava/labsession/CredentialCreator.java
@@ -0,0 +1,12 @@
+package com.greatlearning.corejava.labsession;
+
+import java.util.*;
+
+public class CredentialCreator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
